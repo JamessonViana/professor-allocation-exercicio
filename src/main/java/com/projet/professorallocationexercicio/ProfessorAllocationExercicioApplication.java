9e79d111774a3ee4fbd23d0c1c370ab61b21d06c
Jamesson Viana <jamesson1925@hotmail.com>
2024-02-26 00:35:01 -0300

    Subindo o projeto para o Git

---
+++ src/main/java/com/projet/professorallocationexercicio/ProfessorAllocationExercicioApplication.java
@@ -0,0 +1,14 @@
+package com.projet.professorallocationexercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationExercicioApplication.class, args);
+		//Exercicio java com Spring Bot
+	}
+
+}
---
+++ src/test/java/com/projet/professorallocationexercicio/ProfessorAllocationExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.professorallocationexercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
