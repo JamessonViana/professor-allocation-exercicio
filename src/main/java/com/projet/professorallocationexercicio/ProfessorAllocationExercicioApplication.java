0ec5287bca35a99f2bcfa00e3ed7a64759ba27cc
NairtonMenezes <nairtonmenezesjunior@gmail.com>
2024-02-26 20:06:39 -0300

    teste
    1º subida.

--- src/main/java/com/projet/professorallocationexercicio/ProfessorAllocationExercicioApplication.java
+++ src/main/java/com/projet/professorallocationexercicio/ProfessorAllocationExercicioApplication.java
@@ -1,5 +1,6 @@
 package com.projet.professorallocationexercicio;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class ProfessorAllocationExercicioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationExercicioApplication.class, args);
 		//Exercicio java com Spring Bot
+		System.out.println("teste nairton");
 	}
 
 }
